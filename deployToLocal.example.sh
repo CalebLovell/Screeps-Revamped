244dfa72c2eacf25f2cf4af86eccb742f13ede5e
Caleb Lovell <caleblovell1@gmail.com>
2020-03-17 14:04:22 -0600

    delete useless stuff

---
+++ deployToLocal.example.sh
@@ -0,0 +1,2 @@
+npm run push-pserver
+cp dist/main.js /c/users/"PUT_USERNAME_HERE"/appdata/local/screeps/scripts/127_0_0_1___21025/default
--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,4 @@
-//
-// ___________________________________________________________
-//
-//  _____  _    _ _______  ______ _______ _____ __   _ ______
-// |     |  \  /  |______ |_____/ |  |  |   |   | \  | |     \
-// |_____|   \/   |______ |    \_ |  |  | __|__ |  \_| |_____/
-//
-// _______________________ Screeps AI ________________________
-//
-//
-// Overmind repository: github.com/bencbartlett/overmind
-//
-
 'use strict';
-
-const test = () => { console.log('test2'); };
 
 const spawnCreeps = (gameRoom) => {
     const spawns = Game.rooms[gameRoom].find(FIND_MY_SPAWNS);
@@ -21,15 +6,16 @@ const spawnCreeps = (gameRoom) => {
     spawns[0].spawnCreep([WORK, CARRY, MOVE, MOVE], `Bob`);
 };
 
+const assignTasks = (gameRoom) => {
+};
+
 console.log('test');
 const gameRooms = ['W2N5'];
-test();
 let currentRoom = Game.spawns.Spawn1.room;
 let roomControllerLevel = currentRoom.controller.level;
-// import { assignTasks } from "./src/tasks/task";
 // // import { screepsWork } from "./src/spawners/spawner";
 for (let i = 0; i < gameRooms.length; i++) {
     spawnCreeps(gameRooms[i]);
-    // assignTasks(gameRooms[i]);
+    assignTasks(gameRooms[i]);
     // screepsWork(gameRooms[i]);
 }
