71d7e83867d28dfc353798816c290f4609a06eb6
Marky <marky@mark.com>
2020-03-17 12:20:48 -0700

    Remove push to server

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-cp dist/main.js /c/users/marku/appdata/local/screeps/scripts/10_3_30_51___21025/default
+npm run push-pserver
+cp dist/main.js /c/users/marku/appdata/local/screeps/scripts/127_0_0_1___21025/default
--- dist/main.js
+++ dist/main.js
@@ -13,5 +13,18 @@
 
 'use strict';
 
-"use strict";
+const test = () => { console.log('test2'); };
+
 console.log('test');
+const gameRooms = [`W6N1`];
+test();
+let currentRoom = Game.spawns.Spawn1.room.name;
+// let roomControllerLevel = Game.rooms[currentRoom].controller.level;
+// import { spawnCreeps } from "./src/spawners/spawner";
+// import { assignTasks } from "./src/tasks/task";
+// // import { screepsWork } from "./src/spawners/spawner";
+// for (let i = 0; i < gameRooms.length; i++) {
+// 	spawnCreeps(gameRooms[i]);
+// 	assignTasks(gameRooms[i]);
+// 	// screepsWork(gameRooms[i]);
+// }
