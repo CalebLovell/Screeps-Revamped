491f8fe0606ed23fc8d200d7253f4ac4a277f248
Marky <marky@mark.com>
2020-03-17 12:48:02 -0700

    Remove node_modules

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ test()
 let currentRoom = Game.spawns.Spawn1.room;
 let roomControllerLevel = currentRoom.controller!.level
 import { spawnCreeps } from "spawners/spawner";
-// import { assignTasks } from "./src/tasks/task";
+import { assignTasks } from "tasks/task";
 // // import { screepsWork } from "./src/spawners/spawner";
 
 for (let i = 0; i < gameRooms.length; i++) {
 	spawnCreeps(gameRooms[i]);
-	// assignTasks(gameRooms[i]);
+	assignTasks(gameRooms[i]);
 	// screepsWork(gameRooms[i]);
 }
